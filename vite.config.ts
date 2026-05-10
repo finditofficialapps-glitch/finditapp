1e09fca0869566a98bc00565e0d79314f6874ab6
finditofficialapps-glitch <finditofficialapps@gmail.com>
2026-05-10 11:01:37 +0900

    Fix Vite config formatting and remove unused imports

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
-3) src/main.tsx
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
